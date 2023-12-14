2ecb1e896c82026921a3fc73df2d35c0608e7ee2
Jorge310820 <153870212+Jorge310820@users.noreply.github.com>
2023-12-14 16:15:53 -0600

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CONTADOR_MANUAL
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CONTADOR_MANUAL;
+
+public partial class MainPage : ContentPage
+{
+    private int _contador = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private void OnContarButtonClicked(object sender, EventArgs
+   e)
+    {
+        int incrementValue = (int)IncrementSlider.Value;
+        _contador += incrementValue;
+        ConteoLabel.Text = _contador.ToString();
+    }
+    private void OnReiniciarButtonClicked(object sender,
+   EventArgs e)
+    {
+        _contador = 0;
+        ConteoLabel.Text = _contador.ToString();
+    }
+    private void OnIncrementValueChanged(object sender,
+   ValueChangedEventArgs e)
+    {
+        int incrementValue = (int)e.NewValue;
+        ConteoLabel.Text = _contador.ToString();
+        IncrementLabel.Text = $"Incremento: {incrementValue}";
+    }
+}
